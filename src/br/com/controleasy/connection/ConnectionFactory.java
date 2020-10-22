61197b04e7bd5410433a56b942c21b70f226aef8
Luan Paiva <luanpaivaoficial@gmail.com>
2020-10-21 22:53:41 -0300

    commit 2020-10-21 / 3 Beta

--- src/br/com/controleasy/connection/ConnectionFactory.java
+++ src/br/com/controleasy/connection/ConnectionFactory.java
@@ -5,9 +5,14 @@
  */
 package br.com.controleasy.connection;
 
+import br.com.controleasy.util.Alerts;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import javafx.scene.control.Alert;
 
 /**
  *
@@ -17,10 +22,24 @@ public class ConnectionFactory {
 
     private static Connection connection;
 
+    public static Properties getProperties() {
+        try {
+            Properties properties = new Properties();
+            FileInputStream file = new FileInputStream("C:/Program Files/Controleasy/properties/config.properties");
+            properties.load(file);
+            return properties;
+        } catch (IOException e) {
+            Alerts.showAlert("Controleasy", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+        return null;
+    }
+
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://192.168.1.60:3306/controleasy?useSSL=false&userTimezone=true&serverTimezone=America/Sao_Paulo", "root", "root");
+            Properties properties = getProperties();
+            String ip = properties.getProperty("ip_server_mysql");
+            connection = DriverManager.getConnection("jdbc:mysql://" + ip + "/controleasy?useSSL=false&userTimezone=true&serverTimezone=America/Sao_Paulo", "root", "root");
             return connection;
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
