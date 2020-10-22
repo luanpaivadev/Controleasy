c705717162255a6c1aeafffcdba95a8ee909214a
Luan Paiva <luanpaivaoficial@gmail.com>
2020-10-21 22:40:02 -0300

    Revert "commit 2020-10-21 / 2"
    This reverts commit 08f50a91005204067d68469a7dd0cd999ebcfbc0.

--- src/br/com/controleasy/connection/ConnectionFactory.java
+++ src/br/com/controleasy/connection/ConnectionFactory.java
@@ -5,14 +5,9 @@
  */
 package br.com.controleasy.connection;
 
-import br.com.controleasy.util.Alerts;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-import javafx.scene.control.Alert;
 
 /**
  *
@@ -22,27 +17,13 @@ public class ConnectionFactory {
 
     private static Connection connection;
 
-    public static Properties getProperties() {
-        try {
-            Properties properties = new Properties();
-            FileInputStream file = new FileInputStream("C:/Program Files/Controleasy/properties/config.properties");
-            properties.load(file);
-            return properties;
-        } catch (IOException e) {
-            Alerts.showAlert("Controleasy", null, e.getMessage(), Alert.AlertType.ERROR);
-        }
-        return null;
-    }
-
     public static Connection getConnection() {
         try {
-            Properties properties = getProperties();
-            String ip = properties.getProperty("ip_server");
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://" + ip + "/controleasy?useSSL=false&userTimezone=true&serverTimezone=America/Sao_Paulo", "root", "root");
+            connection = DriverManager.getConnection("jdbc:mysql://192.168.1.60:3306/controleasy?useSSL=false&userTimezone=true&serverTimezone=America/Sao_Paulo", "root", "root");
             return connection;
         } catch (ClassNotFoundException | SQLException e) {
-            Alerts.showAlert("Controleasy", null, e.getMessage(), Alert.AlertType.ERROR);
+            System.out.println(e.getMessage());
         }
         return null;
     }
@@ -53,7 +34,7 @@ public class ConnectionFactory {
                 connection.close();
             }
         } catch (SQLException e) {
-            Alerts.showAlert("Controleasy", null, e.getMessage(), Alert.AlertType.ERROR);
+            System.out.println(e.getMessage());
         }
     }
 }
